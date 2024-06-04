42cc97fdd2d708719a3a34280f27ea4be67c3b9d
santiago <santiago@iMac-de-santiago.local>
2024-06-04 18:26:36 -0500

    agregando todos los header necesarios para que Uci-Cal-Api funcione, falta agregar el archivo StarterKitCALConfig.xml para que nuestra conexion se ejecute

---
+++ starterKit/src/asb_uci/base/build/CMakeFiles/my_example.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/santiago/Desktop/UBU/Uci-Cal-api/starterKit/src/asb_uci/base/AbstractServiceBusConnection.cpp" "CMakeFiles/my_example.dir/AbstractServiceBusConnection.cpp.o" "gcc" "CMakeFiles/my_example.dir/AbstractServiceBusConnection.cpp.o.d"
+  "/Users/santiago/Desktop/UBU/Uci-Cal-api/starterKit/src/asb_uci/base/ConfigReader.cpp" "CMakeFiles/my_example.dir/ConfigReader.cpp.o" "gcc" "CMakeFiles/my_example.dir/ConfigReader.cpp.o.d"
+  "/Users/santiago/Desktop/UBU/Uci-Cal-api/starterKit/src/asb_uci/base/ConnectionMonitor.cpp" "CMakeFiles/my_example.dir/ConnectionMonitor.cpp.o" "gcc" "CMakeFiles/my_example.dir/ConnectionMonitor.cpp.o.d"
+  "/Users/santiago/Desktop/UBU/Uci-Cal-api/starterKit/src/asb_uci/base/ConnectionStatus.cpp" "CMakeFiles/my_example.dir/ConnectionStatus.cpp.o" "gcc" "CMakeFiles/my_example.dir/ConnectionStatus.cpp.o.d"
+  "/Users/santiago/Desktop/UBU/Uci-Cal-api/starterKit/src/asb_uci/base/UUIDGenerator.cpp" "CMakeFiles/my_example.dir/UUIDGenerator.cpp.o" "gcc" "CMakeFiles/my_example.dir/UUIDGenerator.cpp.o.d"
+  "/Users/santiago/Desktop/UBU/Uci-Cal-api/starterKit/src/asb_uci/base/example.cpp" "CMakeFiles/my_example.dir/example.cpp.o" "gcc" "CMakeFiles/my_example.dir/example.cpp.o.d"
+  "/Users/santiago/Desktop/UBU/Uci-Cal-api/starterKit/src/asb_uci/base/main.cpp" "CMakeFiles/my_example.dir/main.cpp.o" "gcc" "CMakeFiles/my_example.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ starterKit/src/asb_uci/base/build/CMakeFiles/my_example.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/my_example.dir/AbstractServiceBusConnection.cpp.o"
+  "CMakeFiles/my_example.dir/AbstractServiceBusConnection.cpp.o.d"
+  "CMakeFiles/my_example.dir/ConfigReader.cpp.o"
+  "CMakeFiles/my_example.dir/ConfigReader.cpp.o.d"
+  "CMakeFiles/my_example.dir/ConnectionMonitor.cpp.o"
+  "CMakeFiles/my_example.dir/ConnectionMonitor.cpp.o.d"
+  "CMakeFiles/my_example.dir/ConnectionStatus.cpp.o"
+  "CMakeFiles/my_example.dir/ConnectionStatus.cpp.o.d"
+  "CMakeFiles/my_example.dir/UUIDGenerator.cpp.o"
+  "CMakeFiles/my_example.dir/UUIDGenerator.cpp.o.d"
+  "CMakeFiles/my_example.dir/example.cpp.o"
+  "CMakeFiles/my_example.dir/example.cpp.o.d"
+  "CMakeFiles/my_example.dir/main.cpp.o"
+  "CMakeFiles/my_example.dir/main.cpp.o.d"
+  "my_example"
+  "my_example.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/my_example.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
